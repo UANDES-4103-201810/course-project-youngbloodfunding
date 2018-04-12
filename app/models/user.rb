d24f3ec0e8651166c635ed712ff857af4a9ba61b
gtroncossi <gtroncossi@miuandes.cl>
2018-04-12 18:29:46 -0300

    Added user and project validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :name, length: { in: 2..30 }, presence: true
+  validates :last_name, length: { in: 2..30 }, presence: true
+  validates :password, length: { minimum: 8 }, presence: true
+  validates :email, presence: true #AGREGAR VALIDACION DE CORREO
+  validates :role, numericality: { only_integer: true }, presence: true
   has_many :bank_accounts
   has_many :promises
   has_many :backed_projects, through: :backers
--- db/migrate/20180412014837_create_users.rb
+++ db/migrate/20180412014837_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :lats_name
+      t.string :last_name
       t.integer :role
       t.string :email
       t.string :password
